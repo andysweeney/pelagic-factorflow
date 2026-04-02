42bfbcb4e93704725d23d68b86e5f110c92a9cbc
andysweeney <andy@sweeneymail.co.uk>
2026-04-02 16:30:25 +0100

    Create supabaseClient.js

---
+++ src/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+var supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+var supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+export var supabase = createClient(supabaseUrl, supabaseAnonKey)
